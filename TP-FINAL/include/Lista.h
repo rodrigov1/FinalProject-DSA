f8c32ed8a8c9db5ecc3b63c3f0f85fa0c0719ff4
Simøn <simonsaillen29@gmail.com>
2024-01-16 19:44:34 -0300

    Probando los Include

--- TP-FINAL/include/Lista.h
+++ TP-FINAL/include/Lista.h
@@ -1,7 +1,6 @@
 #ifndef LISTA_H
 #define LISTA_H
-#include <iostream>
-#include <string>
+
 #include "Nodo.h"
 
 using namespace std;
--- TP-FINAL/src/main.cpp
+++ TP-FINAL/src/main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <Lista.h>
+#include <Nodo.h>
+
 using namespace std;
 
 int main()
