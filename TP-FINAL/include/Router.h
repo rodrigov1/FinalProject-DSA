365e0588dc38d74e98decf8d52c0a87620a0962a
Simøn <simonsaillen29@gmail.com>
2024-01-17 18:09:11 -0300

    new header files

---
+++ TP-FINAL/include/Router.h
@@ -0,0 +1,17 @@
+#include <string>
+#include "Cola.h"
+
+using namespace std;
+
+class Router {
+    private:
+    byte id;
+    byte terminal;
+
+    public:
+    Router(){;};
+    //~Router();
+    void setId();
+    byte getId();
+    
+};
---
+++ TP-FINAL/src/Router.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "../include/Router.h"
+
+using namespace std;
+
+// class methods 
