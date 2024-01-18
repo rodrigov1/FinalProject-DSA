43587a9b30b5c5a7eaa999647805c4b054c5d517
Simøn <simonsaillen29@gmail.com>
2024-01-18 13:25:02 -0300

    small fixes

---
+++ TP-FINAL/include/Paquetes.h
@@ -0,0 +1,13 @@
+#ifndef PAQUETES_H
+#define PAQUETES_H
+
+using namespace std;
+
+class Paquetes {
+    private:
+    int id;
+
+    public:
+};
+
+#endif
--- TP-FINAL/src/Router.cpp
+++ TP-FINAL/src/Router.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include "../include/Router.h"
 #include "../include/Lista.h"
