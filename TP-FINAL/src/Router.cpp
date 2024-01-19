d944305f85172e98b90e9eb9a677eaa48b530b13
Simøn <simonsaillen29@gmail.com>
2024-01-19 20:37:42 -0300

    Update Router.cpp

--- TP-FINAL/src/Router.cpp
+++ TP-FINAL/src/Router.cpp
@@ -1,19 +1,13 @@
+#include <iostream>
 #include <string>
 #include "../include/Router.h"
+#include "../include/Lista.h"
+#include "../include/Pagina.h"
+#include "../include/Terminal.h"
 
 using namespace std;
 
-// class methods 
-
-Cola<Paquetes*> Router :: dividePage(Pagina page) {
-    Cola<Paquetes*> aux;
-    int size = page.getSize();   // n paquetes del mismo tamaño (m.c.m pero hay que implementarlo)
-
-    for(int i = 0; i < size; i++) {
-        Paquetes *pkg = new Paquetes(i,page);
-        aux.encolar(pkg);
-        delete pkg;
-    }
-
-    return aux;
-};
+void Router::add_terminal(Terminal *t)
+{
+    terminales_conectados->addFinal(t);
+}
